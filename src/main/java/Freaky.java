ce7f14c1ce6002f4faf74b1c28de07ad835f9760
FreakkMe <kuekyihong@gmail.com>
2026-01-28 04:11:46 +0800

    Interface improved, echo and bye features are added

--- src/main/java/Freaky.java
+++ src/main/java/Freaky.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
 public class Freaky {
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm Freaky. \n"
-                         + "What can I do for you? \n"
-                         + "Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+
+        String greet = "----------------------------------------------------- \n"
+                     + "Hello! I'm Freaky. \n"
+                     + "What can I do for you? \n"
+                     + "----------------------------------------------------- \n";
+
+        String bye = "----------------------------------------------------- \n"
+                   + "Bye. Hope to see you again soon! \n"
+                   + "----------------------------------------------------- \n";
+
+        System.out.println(greet);
+
+        String input;
+
+        while(true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println("----------------------------------------------------- \n"
+                                 + input + "\n"
+                                 + "----------------------------------------------------- \n");
+            }
+        }
+
+        System.out.println(bye);
 
     }
 }
