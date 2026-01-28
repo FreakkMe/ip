643fcd6d85e736ff26822c7e735f01f2f6ba87c0
FreakkMe <kuekyihong@gmail.com>
2026-01-28 13:20:53 +0800

    Comments added to existing codes

--- src/main/java/Freaky.java
+++ src/main/java/Freaky.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Freaky {
 
+    // Print method to print a string with format bar on top and below
     public static void print(String string) {
         System.out.println("----------------------------------------------------- \n"
                          + string
@@ -10,23 +11,31 @@ public class Freaky {
 
     public static void main(String[] args) {
 
+        // Input of the useer
         Scanner scanner = new Scanner(System.in);
 
+        // Greet message
         String greet = "Hello! I'm Freaky. \n"
                      + "What can I do for you? \n";
 
+        // Bye message
         String bye = "Bye. Hope to see you again soon! \n";
 
+        // Chatbot starts here
         print(greet);
 
+        // Initialize variables input to store user's previous input, list to store all user's input and count to store number of user's input
         String input;
         String list = "";
         int count = 0;
 
+        // Detecting user's input
         while(true) {
 
+            // Stores user's input to input
             input = scanner.nextLine();
 
+            // Checks user's input of different cases: "bye", "list" and others
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
