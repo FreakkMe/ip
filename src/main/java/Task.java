216887f365a4e3dae4bca5c53b957250192cf63d
FreakkMe <kuekyihong@gmail.com>
2026-01-28 13:27:45 +0800

    Task class added

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+
+    // String description stores description of the task, boolean isDone stores the status of the task
+    protected String description;
+    protected boolean isDone;
+
+    // Constuctor of task, default to be marked as undone
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    // Returns the status of the task
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    // Marks the task as done
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+}
